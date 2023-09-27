6a10da672859027be391bfa225ef1dd1b8479500
ozeddy2354 <142768114+ozeddy2354@users.noreply.github.com>
2023-09-27 06:31:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//console.log('hello world')
+//let name= 'mash';
+//console.log(name); 
+// cant be used like te reverse key word 
+// reverse key word eg let ,if,
+//they should be mining full
+//can not  start wit the number
+//ver are case sensitive 
+//let interestRate=0.3;
+//interestRate = 1;
+//console.log(interestRate);
+
+//object
+
+//let person= {
+    //name:'mash',
+  //  Age: 23
+
+//};
+// dot notation 
+//person.name='john'
+//console.log(person.name)
+//braket notatation 
+//person[name]='mary'
+//console.log(person[name])
+//console.log(person)
+
+//Array
+//let colrs=['red','blue','green','yellow']
+//colrs[5]='brown'
+//console.log(colrs)
+//console.log(colr[0])
+
+//function
+
+function  greet(name){
+    console.log('hello',+name);
+}
+greet('john');
+// culculation 
+function square (number){
+ return   number*number;
+}
+
+console.log(square(3));
